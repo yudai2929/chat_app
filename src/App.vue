7c9fd403c8518fa2a3362061eb59092f43bd4ce7
Yudai Koike <yudai.koike@e-seikatsu.co.jp>
2022-09-06 17:10:32 +0900

    refs #12 レイアウト調整

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,8 @@
-<script lang="ts" setup>
-import { getAuth, signOut } from 'firebase/auth';
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-const signOutWithGoogle = () => {
-  const auth = getAuth();
-  signOut(auth)
-    .then(() => {
-      router.push({ path: '/login' });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-</script>
-
 <template>
   <div>
-    <el-menu
-      default-active="home"
-      class="el-menu-demo"
-      mode="horizontal"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-      :ellipsis="false"
-      router
-    >
-      <el-menu-item index="0"
-        >2022サマーインターンボイラープレート</el-menu-item
-      >
-      <div class="flex-grow"></div>
-      <el-menu-item index="home">Home</el-menu-item>
-      <el-menu-item index="about">About</el-menu-item>
-      <el-menu-item index="login">Login</el-menu-item>
-      <el-button @click="signOutWithGoogle">sign out</el-button>
-    </el-menu>
-    <RouterView />
+    <default-header />
+    <main style="padding-top: 60px; min-height: 100vh">
+      <RouterView />
+    </main>
   </div>
 </template>
-
-<style scoped>
-.flex-grow {
-  flex-grow: 1;
-}
-</style>
