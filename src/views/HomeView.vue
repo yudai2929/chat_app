7402bde620d31c6a2bc6a90eb99cad4e1658090e
Sasaki Toui <toui@101.local>
2022-09-07 10:16:23 +0900

    refs #11 検索部分の実装

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,27 @@
 <template>
+  <div style="margin: 50px 50px; font-size: 30px; text-align: center">
+    上京したてでお金のない大学生でも大丈夫！<span
+      >楽器の弾ける格安賃貸をご案内</span
+    >
+  </div>
   <ElRow justify="center">
     <ElCol :span="30">
-      <div style="text-aligin: center">テスト</div>
-      <label for="free word">フリーワード検索</label>
-      <ElInput v-model="text" type="text" />
-      <el-button type="primary" :icon="Search" @click="onSearch"
-        >検索</el-button
-      >
-      <el-button type="info"
-        ><router-link :to="{ name: 'search' }">絞り込み</router-link></el-button
-      >
-      <div v-if="queryResult">
-        <BukkenPropertyCard
-          v-for="res in queryResult.results"
-          :key="res.buildingGuid"
-          :building-property-preview="res"
-        />
+      <div style="text-align: center; float: left; margin: 50px 50px">
+        フリーワードで検索
+        <ElInput v-model="text" type="text" />
+        <el-button type="primary" :icon="Search" @click="onSearch"
+          >検索</el-button
+        >
+      </div>
+      <div style="text-align: center; float: right; margin: 50px 50px">
+        大学名で検索
+        <ElInput v-model="area" type="text" />
+        <el-button type="primary" :icon="Search" @click="onSearch_area"
+          >検索</el-button
+        >
       </div>
     </ElCol>
+    <!--ここに地図上での表示をお願いします-->
   </ElRow>
 </template>
 
@@ -40,5 +44,16 @@ const onSearch = async () => {
   queryResult.value = await rentPropertyQueryAPI.searchRentPropertyByBuilding({
     buildingName: text.value,
   });
+};
+
+const area = ref('');
+// dejimaAPIクライアントを初期化
+const queryResult_area = ref<BuildingPropertyList | null>(null);
+const onSearch_area = async () => {
+  // 検索ボタンが押されたらinputフォームの入力で物件を検索し、結果を`queryResult`に代入する
+  queryResult_area.value =
+    await rentPropertyQueryAPI.searchRentPropertyByBuilding({
+      buildingName: area.value,
+    });
 };
 </script>
